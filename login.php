4fa6e4dbe9bc5259fd30c694085070b60a916b16
Gunavarthan <141598957+Gunavarthan@users.noreply.github.com>
2024-06-21 20:44:26 +0530

    Update login.php
    > Time Zone 
    > Addition of new records to tickets

--- login.php
+++ login.php
@@ -30,25 +30,33 @@
             </div>
         </form>
         <?php
+            date_default_timezone_set('Asia/Kolkata');                         //defining the TimeZone
             $con = mysqli_connect("localhost", "root", "guna", "train");
 
             if (isset($_POST["PNR"])) {
                 $PNR = mysqli_real_escape_string($con, $_POST["PNR"]);
+                $query = "delete from tickets where TicketID = 'UnInit'";       //Checks for non initialized records and delete 
+                mysqli_query($con, $query);
                 $query = "SELECT * FROM pnr WHERE PNR='$PNR'";                   //selecting rows with same PNR value
                 $result = mysqli_query($con, $query);
 
-                if (mysqli_num_rows($result) > 0) {                                                 //if Result rows is greater than 0 then Redirect to Next Page
+                if (mysqli_num_rows($result) > 0) {                                                 //if Result rows is greater than 0  
                     $row = mysqli_fetch_assoc($result);
 
                     $Today = date('Y-m-d');                         //Current date
                     $CurrTime = strtotime(date('H:i:s'));           //Current Time
                     $travelTime = strtotime($row['TravelTime']);    //time of travel
-                    $validTime = $travelTime - 4 * 3600;            //Four hours before travel
+                    $validTime = $travelTime - 2 * 3600;            //Two hours before travel
+                    $validEndTime = $travelTime + 1 * 3600;         // One hour after travel
 
-                    if($row['TravelDate'] == $Today && $CurrTime >= $validTime && $CurrTime <= $travelTime ){                 //PNR with Date as same and time just 4hs before the travel time are permitted 
+                    if($row['TravelDate'] == $Today && $CurrTime >= $validTime && $CurrTime <= $validEndTime){                 //PNR with Date as same and time just 2hrs before or 1hr after the travel time are Redirect to Next Page 
+                        $query = "insert into tickets (TicketID,pnr,NumberOfGuests,NumberOfAdults,NumberOfChildren,Price,PlatformNumber) values ('UnInit',$PNR,-0101,-0101,-0101,-0101,-0101)";
+                        echo("$CurrTime >= $validTime   $CurrTime <= $validEndTime");
+                        mysqli_query($con, $query);
                         echo "<script>window.location.href='ticketinput.php';</script>";         
                     } 
                     else{
+                        echo("$CurrTime >= $validTime   $CurrTime <= $validEndTime");
                         echo "<div class='alert'>PNR schedule mismatch </div>";    
                     }
                 } else {
